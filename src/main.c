8016212e6f2204842bb0595f9b2cfc7761d3f392
Fayet Mathis <fmathis@localhost.localdomain>
2019-11-27 23:05:30 +0100

    All my main for bsq

--- src/main.c
+++ src/main.c
@@ -11,24 +11,22 @@ int my_getnbr(char const *str);
 
 int main(int ac, char **av)
 {
-    char *buffer;
-    int length;
-    int filez;
-    int fd;
-    struct stat info;
-
     if (ac != 2)
-        return (84);
-    stat(av[1], &info);
-    length = info.st_size;
-    buffer = malloc(sizeof(char) * length + 2);
+        return(84);
+    struct stat tab;
+    int fd = open(av[1], O_RDONLY);
+    stat(av[1], &tab);
+    int lenght = tab.st_size;
+    char *buffer = malloc(sizeof(char) * (lenght + 2));
+    int back = read(fd, buffer, lenght);
     int nbline = my_getnbr(buffer);
-    int **map = malloc(sizeof(int *) * nbline + 1);
-    fd = open(av[1], O_RDONLY);
-    filez = read(fd, buffer, length);
-    buffer[length + 1] = '\0';
+    int **map = malloc(sizeof(int *) * (nbline + 1));
+    char *bufferback = buffer;
 
-    if (length <= 0)
+    buffer[lenght+1] = '\0';
+    if (back <= 0 || nbline <= 0 || lenght <= 0 || fd <= 0)
         return (84);
-    check_map(buffer, nbline, map, length);
+    check_map(bufferback, nbline, map, lenght);
+    close(fd);
+    free(buffer);
 }
