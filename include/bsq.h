7df1c379851cc5eefbf2496361b86210f25ac938
Fayet Mathis <fmathis@localhost.localdomain>
2019-11-27 23:03:42 +0100

    Push my include for my .h

--- include/bsq.h
+++ include/bsq.h
@@ -21,10 +21,16 @@ typedef struct bsq
     int nb;
 } tab_t;
 
-int **mall(int **map, char *buffer, int nbline);
-int **remplace_tab(char *buffer, int **map);
+static int **mall(int **map, char *buffer, int nbline);
+static int **remplace_tab(char *buffer, int **map);
 void check_map(char *buffer, int nbline, int **map, int length);
 int main(int ac, char **av);
 int my_getnbr(char const *str);
+int numbercharbylign(char *buffer);
+static int **remplace_tab(char *buffer, int **map);
+void replacefunction(char *buffer, tab_t *bsq, int nbline, int lenght);
+int calc_min(int a, int b, int c);
+void check_struct(int nb, int x, int y, tab_t *bsq);
+static void check_error(char *buffer, int nbline);
 
 #endif /* BSQ_H */
